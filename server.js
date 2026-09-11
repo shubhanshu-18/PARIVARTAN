88d00d4c025f6d2477f613a86d1b2b877444f0fc
Shubhanshu-18 <shubhtripathi1438@gmail.com>
2026-09-11 19:12:08 +0530

    added url to server

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(
 // CORS setup
 app.use(
   cors({
-    origin: "*",
+    origin: "https://parivartan-tau.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
